a87683b1bbd195a2d41752ce5459d3b8d264af4c
Natig Mammadov <Natig.Mammadov@younicos.com>
2019-08-15 04:46:02 +0200

    Boot starter class added.

---
+++ src/main/java/com/task/food/FoodsApplication.java
@@ -0,0 +1,12 @@
+package com.task.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoodsApplication.class, args);
+  }
+}
